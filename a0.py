cd140358cd023ca3fad5546ab8dadb887686776a
Michelle Ly <lym8@uci.edu>
2023-01-13 14:46:59 -0800

    test run1

---
+++ a0.py
@@ -0,0 +1,9 @@
+# a0.py
+
+# Starter code for assignment 0 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# NAME
+# EMAIL
+# STUDENT ID
