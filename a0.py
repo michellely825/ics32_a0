fafa186d1533397e70989202ae179a5ab1980f3e
Michelle Ly <lym8@uci.edu>
2023-01-13 15:20:56 -0800

    created fx

--- a0.py
+++ a0.py
@@ -4,6 +4,28 @@
 
 # Replace the following placeholders with your information.
 
-# NAME
-# EMAIL
-# STUDENT ID
+# Michelle Ly
+# lym8@uci.edu
+# 62717883
+
+userInput = int(input())
+
+def downwardBlock():
+    first = "+-+"
+    second = "| |"
+    third = "+-+-+"
+    indent = "  "
+    if i == 0:
+        print(first)
+        print(second)
+        print(third)
+    elif i == userInput - 1:
+        print(f'{indent * i}{second}')
+        print(f'{indent * i}{first}')
+    else:
+        print(f'{indent * i}{second}')
+        print(f'{indent * i}{third}')
+
+for i in range(userInput):
+    downwardBlock()
+
